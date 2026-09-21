b6049afbfbfe22bfdcf4454ab3cfc055d51f466d
rebeccadimartini-art <rebecca.dimartini@escola.pr.gov.br>
2026-09-21 09:31:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+header {
+background-color:#e1d1c6
+  color: #795138;
+  tex-aling:center:center;
+  max-width: 800px;
+  margin: 0 auto;
+  border: 5px solid #FFE7BA;
+  padding: 16px;
+}
 
+main{
+  background-color:#FFDAB9
+    
